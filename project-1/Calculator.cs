c0765a42d89781d8530d68d231d7f3b0fd1215ee
KempDewulf <kemp.dewulf@hotmail.com>
2023-03-25 02:11:32 +0100

    add fields

--- project-1/Calculator.cs
+++ project-1/Calculator.cs
@@ -1,6 +1,15 @@
-﻿namespace project_1;
+﻿using System.Security.Cryptography;
+
+namespace project_1;
 
 public class Calculator
 {
-   private string input;
+   string input = "0";
+   
+   string num1 = "0";
+
+   string num2 = "0";
+
+   string function = string.Empty;
+   
 }
