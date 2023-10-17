367d24b845b15c294210d3de06c37491945fe648
kemp.dewulf <kemp.dewulf@student.howest.be>
2023-10-17 19:46:02 +0200

    add basic calc methods

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,16 @@
+package calculator.domain;
+
+public class Calculator {
+    public static float add(float num1, float num2) {
+        return num1 + num2;
+    }
+    public static float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+    public static float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+    public static float divide(float num1, float num2) {
+        return num1 / num2;
+    }
+}
--- src/main/java/calculator/services/CalculatorService.java
+++ src/main/java/calculator/services/CalculatorService.java
@@ -1,4 +1,28 @@
 package calculator.services;
 
 public class CalculatorService {
+    public String add(String num1, String num2) {
+        float float1 = Float.parseFloat(num1);
+        float float2 = Float.parseFloat(num2);
+
+        return Float.toString(float1 + float2);
+    }
+    public String subtract(String num1, String num2) {
+        float float1 = Float.parseFloat(num1);
+        float float2 = Float.parseFloat(num2);
+
+        return Float.toString(float1 - float2);
+    }
+    public String multiply(String num1, String num2) {
+        float float1 = Float.parseFloat(num1);
+        float float2 = Float.parseFloat(num2);
+
+        return Float.toString(float1 * float2);
+    }
+    public String divide(String num1, String num2) {
+        float float1 = Float.parseFloat(num1);
+        float float2 = Float.parseFloat(num2);
+
+        return Float.toString(float1 / float2);
+    }
 }
