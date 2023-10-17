32b94eb5924b8c1ec0e007cd8d35950ae1f97e0b
kemp.dewulf <kemp.dewulf@student.howest.be>
2023-10-17 19:23:45 +0200

    add title and icon

--- src/main/java/calculator/application/CalculatorApp.java
+++ src/main/java/calculator/application/CalculatorApp.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
@@ -20,6 +22,8 @@ public class CalculatorApp extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
+        primaryStage.setTitle("Calculator");
+        primaryStage.getIcons().add(new Image("file:src/main/resources/icons/calculator.png"));
         primaryStage.show();
     }
 }
