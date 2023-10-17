1c7a393757bd0bcdd5038efe2fabf86ebf4adb0f
kemp.dewulf <kemp.dewulf@student.howest.be>
2023-10-17 19:58:03 +0200

    replace individual number click events by single clickevent

--- src/main/java/calculator/gui/CalculatorAppController.java
+++ src/main/java/calculator/gui/CalculatorAppController.java
@@ -4,21 +4,59 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class CalculatorAppController {
-
     @FXML
     private ResourceBundle resources;
-
     @FXML
     private URL location;
-
+    @FXML
+    private Button num0;
+    @FXML
+    private Button num1;
+    @FXML
+    private Button num2;
+    @FXML
+    private Button num3;
+    @FXML
+    private Button num4;
+    @FXML
+    private Button num5;
+    @FXML
+    private Button num6;
+    @FXML
+    private Button num7;
+    @FXML
+    private Button num8;
+    @FXML
+    private Button num9;
     @FXML
     private Label outputLbl;
 
     @FXML
     void onAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSubtract(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMultiply(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDivide(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPowerOfTwo(ActionEvent event) {
 
     }
 
@@ -38,85 +76,30 @@ public class CalculatorAppController {
     }
 
     @FXML
-    void onDivide(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onEight(ActionEvent event) {
-
-    }
-
-    @FXML
     void onEnter(ActionEvent event) {
 
     }
 
     @FXML
-    void onFive(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onFour(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onMultiply(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onNine(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onOne(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onPowerOfTwo(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onSeven(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onSix(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onSubtract(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onThree(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onTwo(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onZero(ActionEvent event) {
+    void onNumClick(ActionEvent event) {
 
     }
 
     @FXML
     void initialize() {
+        assert num0 != null : "fx:id=\"num0\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num1 != null : "fx:id=\"num1\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num2 != null : "fx:id=\"num2\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num3 != null : "fx:id=\"num3\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num4 != null : "fx:id=\"num4\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num5 != null : "fx:id=\"num5\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num6 != null : "fx:id=\"num6\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num7 != null : "fx:id=\"num7\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num8 != null : "fx:id=\"num8\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
+        assert num9 != null : "fx:id=\"num9\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
         assert outputLbl != null : "fx:id=\"outputLbl\" was not injected: check your FXML file 'CalculatorApp.fxml'.";
 
-
     }
+
 
 }
--- src/main/java/calculator/services/CalculatorService.java
+++ src/main/java/calculator/services/CalculatorService.java
@@ -1,6 +1,16 @@
 package calculator.services;
 
 public class CalculatorService {
+    private String input;
+
+    public void addInput(String input) {
+        this.input += input;
+    }
+
+    public String getInput() {
+        return this.input;
+    }
+
     public String add(String num1, String num2) {
         float float1 = Float.parseFloat(num1);
         float float2 = Float.parseFloat(num2);
